04ec3a43ae3e94afff5346d1d752974d71db4641
David Wengier <git@wengier.com>
2019-04-15 20:51:29 +1000

    Interface implementation without implementing

---
+++ InterfaceWithoutImplenting/Example.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpQuirks.InterfaceWithoutImplenting
+{
+    public static class Example
+    {
+        public static void DoSomething()
+        {
+            IFoo foo = new Bar();
+            foo.BazMethod();
+        }
+    }
+
+    public interface IFoo
+    {
+        void BazMethod();
+    }
+
+    public class Baz
+    {
+        public void BazMethod()
+        {
+        }
+    }
+
+    public class Bar : Baz, IFoo
+    {
+    }
+}
---
+++ InterfaceWithoutImplenting/ICountable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpQuirks.InterfaceWithoutImplenting
+{
+    public interface ICountable
+    {
+        int Count { get; }
+    }
+
+    public class MyDictionary<T, V> : Dictionary<T, V>, ICountable
+    {
+    }
+
+    public class Example2
+    {
+        public Example2()
+        {
+            var dict = new MyDictionary<string, string>()
+            {
+                ["adsf"] = "qwre",
+                ["poi"] = "hih"
+            };
+
+            CountEm(dict);
+        }
+
+        private void CountEm(ICountable countable)
+        {
+            Console.WriteLine(countable.Count);
+        }
+    }
+}
