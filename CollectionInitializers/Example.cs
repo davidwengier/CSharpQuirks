bae34d13f006a9cecd17adb004555b1e168401ff
David Wengier <git@wengier.com>
2019-04-16 21:42:19 +1000

    Collection Initializers

---
+++ CollectionInitializers/Example.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpQuirks.CollectionInitializers
+{
+    public static class Example
+    {
+        public static void DoSomething()
+        {
+            var list = new List<string>
+            {
+                "Example 1",
+                "Example 2"
+            };
+
+            var dict = new Dictionary<string, string>
+            {
+                { "Key1", "Value1" },
+                { "Key2", "Value2" }
+            };
+
+            var dict2 = new Dictionary<string, string>
+            {
+                ["Key1"] = "Value1",
+                ["Key2"] = "Value2"
+            };
+        }
+
+        public static void CustomAdd()
+        {
+            var foo = new MyThings
+            {
+                { 1, "My Thing 1" },
+                { 2, "My Thing 2" },
+                { 1, "My Other Thing 3" }
+            };
+        }
+    }
+}
---
+++ CollectionInitializers/MyThings.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+
+namespace CSharpQuirks.CollectionInitializers
+{
+    public class MyThings : IEnumerable
+    {
+        public void Add(int number, string value)
+        {
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
